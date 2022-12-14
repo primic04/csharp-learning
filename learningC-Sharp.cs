8ce8365d71c2c387f815da4ee48c5b8158cd1848
primic04 <mtc-dev-programming04@hotmail.com>
2022-12-14 14:55:42 -0500

    ajout

--- learningC-Sharp.cs
+++ learningC-Sharp.cs
@@ -12,15 +12,15 @@ namespace learningCsharp
             Console.WriteLine(homme);
 
 //variable de type int, prend 4 bytes en mémoire
-            int age = 20;
-            Console.WriteLine(age);
+            //int age;
+            //Console.WriteLine(age);
 
 // variable de type char, prend 2 bytes en memoire
             char mon_char = 'a';
             Console.WriteLine(mon_char);
 
 //variable de type string, prend 20+ bytes en memoire
-            string mon_string = "michael";
+            string mon_string = "JeuVideo";
             Console.WriteLine(mon_string);
 
 //variable de type sbyte pour les chiffre entre 0-255, prend en mémoire 1 bytes
@@ -30,6 +30,17 @@ namespace learningCsharp
 //variable de type float, prend en mémoire 4 bytes et on dois mettre un f a la fin du float
             float my_float = 1.44f;
             Console.WriteLine(my_float);
+
+            string prenom = "michael";
+            Console.WriteLine(prenom);
+            string nom = "turcotte-coulombe";
+            Console.WriteLine(nom);
+            Console.WriteLine(prenom + nom);
+
+//un variable peut-être déclaré comme suit
+            int age;
+            age = 27;
+            Console.WriteLine(age);
 
 
            Console.ReadKey(); 
