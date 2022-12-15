8de80c30bc0c06c1f9dc738a1d5b970640623142
primic04 <mtc-dev-programming04@hotmail.com>
2022-12-15 14:15:33 -0500

    add text form

---
+++ 2learningC-Sharp.cs
@@ -0,0 +1,15 @@
+using System;
+namespace learningCsharp2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+
+
+
+            Console.Read();
+        }
+    }
+}
