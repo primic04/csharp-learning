f1d591e3f95e00cdbcfeaef0434abc77ed73cb0f
primic04 <mtc-dev-programming04@hotmail.com>
2022-12-15 14:42:01 -0500

    add mic profil complete

--- 2learningC-Sharp.cs
+++ 2learningC-Sharp.cs
@@ -5,8 +5,23 @@ namespace learningCsharp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("test");
+            string UserName = "Michael";
+            string UserFamName = "T-C";
+            sbyte UserAge = 27;
+            float UserSize = 1.75f;
+            byte UserWeight = 220;
+            //bool homme = true;
+            string UserSex = "homme";
 
+            Console.WriteLine("VOICI VOTRE PROFIL COMPLET");
+            Console.WriteLine("vous avez: " + UserAge + " ans");
+            Console.WriteLine("un int fait " + (sizeof(int)) + " bytes");
+            Console.WriteLine(UserSize + " metre");
+            Console.WriteLine("votre poids est de: " + UserWeight + " lbs");
+            Console.WriteLine(UserSex);
+            Console.WriteLine("votre nom complet est: " + UserName + " " + UserFamName);
+
+            Console.WriteLine("PROFIL COMPLETE...");
 
 
             Console.Read();
