8a2f90a80c425417d87e28b933e3bc4bbdb425fd
primic04 <mtc-dev-programming04@hotmail.com>
2022-12-15 11:56:56 -0500

    add operation arithmetique

--- learningC-Sharp.cs
+++ learningC-Sharp.cs
@@ -5,25 +5,34 @@ namespace learningCsharp
     {
         static void Main(string[] args)
         {
+
+
+
+
+  Console.Read(); 
+        }        
+    }
+}
+
+
+            /*
             int nbr = 4;
             int resultat;
 
             resultat = nbr * 10;
 
-            //Console.WriteLine(resultat);
+            Console.WriteLine(resultat);
             string str = "je chosis le nombre "  + nbr + " et est egale a " + resultat;
             Console.WriteLine(str);
-
-
-
-
-
-
-
-
-
-
-            /*
+            
+            int nbr1 = 1;
+            int nbr2 = 2;
+            int nbr3 = 3;
+            int nbr4 = 4;
+            int total;
+            total = nbr2 / nbr1 * (nbr3 + nbr4);
+            Console.WriteLine(total);
+            
             string dan_name = "danny";
             int age_danny = 28;
             float taille_danny = 1.85f;
@@ -45,10 +54,26 @@ namespace learningCsharp
             float taille_danna = 1.60f;
             Console.WriteLine((danna_name) + (age_danna) + (taille_danna));
 //probleme pour espacer les variable dans le console writeline
+
+            int un = 1;
+            int deux = 2;
+            int trois = 3;
+            int quatre = 4;
+            int cinq = 5;
+            int resultat;
+
+            resultat = 1 + (2 * 10) / 4;
+            Console.Write(resultat);
+
+          int nbr1 = 10;
+          int nbr2 = 5;
+          int nbr3 = 2;
+          int resultat;
+          int total;
+
+          resultat = nbr2 * nbr1;
+
+          Console.WriteLine(resultat * nbr3 / (nbr1 / nbr3));// = 20
+          total = resultat * nbr3 / (nbr1 / nbr3) / 8;
+          Console.WriteLine(total);// = 2
             */
-
-
-  Console.Read(); 
-        }        
-    }
-}
